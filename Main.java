9c982b4364918de4ba91fd511a7f320f217a3612
Ryan Hunter <rhunter4@xula.edu>
2018-04-02 22:50:16 -0500

    //the total price of items purchased on date [D]

--- Main.java
+++ Main.java
@@ -9,17 +9,34 @@ public class Main {
         GroceryItem item01 = new GroceryItem();
         GroceryItem item02 = new GroceryItem(LocalDate.now(), "Almond Milk", 15);
         GroceryItem item03 = new GroceryItem(LocalDate.now(), "Oreo", 4);
+
         //create three grocery item objects
         ArrayList<GroceryItem> listOfItems = new ArrayList<>();
         listOfItems.add(item01);
         listOfItems.add(item02);
         listOfItems.add(item03);
+
+        //write grocery item(s) to the file input.txt
         writeToFile(listOfItems);
 
-        //How many items were purchased during the last visit to the grocery store
+        //number of items were purchased during the last visit to the grocery store
         int itemsPurchased = itemsPurchasedLastVisit(listOfItems);
         System.out.println("Items Purchased Last Visit: " + itemsPurchased);
 
+        //the total price of items purchased on date [D]
+        LocalDate localDate = LocalDate.now();
+        int itemsTotalPrice = totalPriceOfItemsPurchased(listOfItems, localDate);
+        System.out.println("Total Price of Purchased Items(last visit): $" + itemsTotalPrice);
+    }
+
+    private static int totalPriceOfItemsPurchased(ArrayList<GroceryItem> listOfItems, LocalDate date) {
+        int totalPriceSum = 0;
+        for (int i = 0; i < listOfItems.size(); i++) {
+            if (listOfItems.get(i).getDate().equals(date)) {
+                totalPriceSum += listOfItems.get(i).getPrice();
+            }
+        }
+        return totalPriceSum;
     }
 
     private static int itemsPurchasedLastVisit(ArrayList<GroceryItem> listOfItems) {
@@ -30,7 +47,6 @@ public class Main {
         return count;
     }
 
-    //write grocery item(s) to the file input.txt
     private static void writeToFile(ArrayList<GroceryItem> itemArrayList) throws FileNotFoundException {
         PrintWriter printWriter = new PrintWriter("input.txt");
         //writes a nullable object to the PrintWriter
